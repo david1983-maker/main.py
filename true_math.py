f0135d086406ccf181e72f26f7468eb433bfed5b
david1983-maker <davidkilidzan94127@gmail.com>
2024-11-19 20:33:17 +0300

    Add files via upload

---
+++ true_math.py
@@ -0,0 +1,20 @@
+# 1st program
+print(9 ** 0.5 * 5)
+
+# 2nd program
+x = 9.99 > 9.98 and 1000 != 1000.1
+print(x)
+
+# 3rd program
+a = 2 * 2 + 2
+b = 2 * (2 + 2)
+c = a == b
+print(c)
+
+# 4th program
+num = '123.456'
+f = float(num)
+p = int(f)
+z = f * 10
+m = int(z)
+print(m % p)
